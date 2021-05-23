c8ade61455b431cdc9c59de80a342d146a029d76
Tim Davis <contact@timdav.is>
2021-05-23 13:17:27 -0700

    start building client cli

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -21,15 +21,32 @@ program
   .description('Forward HTTP traffic from the public Internet to localhost.')
 
 program
-  .command('serve')
+  .command('server')
   .alias('s')
   .description('start a public tunnel server')
-  .option('-p, --port <port>', 'port to listen on', '80')
-  .option('-a, --address <address>', 'ip address to listen on', '0.0.0.0')
-  .option('-h, --host <host>', 'public hostname of the server', 'localhost')
   .requiredOption('-t, --token <token>', 'access token required for clients')
+  .option('-h, --host <host>', 'hostname of the server', 'localhost')
+  .option('-a, --address <address>', 'ip address to listen on', '0.0.0.0')
+  .option('-p, --port <port>', 'port number to listen on', '1111')
   .action(({ token, address, host, port }) => {
-    createServer({ host, token }).listen(port, address)
+    createServer({ host, token }).listen(port, address, () => {
+      // eslint-disable-next-line no-console
+      console.log(`http-public listening at ${address}:${port} for ${host}\n`)
+    })
+  })
+
+program
+  .command('client <serverUrl>', { isDefault: true })
+  .alias('c')
+  .description('start a local tunnel client')
+  .requiredOption('-t, --token <token>', 'access token for the tunnel server')
+  .requiredOption('-d, --subdomain <name>', 'subdomain for the tunneled app')
+  .option('-p, --local-port <port>', 'port number of the local app', '3000')
+  .option('-l, --local-host <host>', 'hostname of the local app', 'localhost')
+  .option('--secure', 'use https to connect to the local app', false)
+  .option('--verbose', 'log requests arriving through the tunnel', false)
+  .action((serverUrl, { token, secure, localHost, localPort, verbose }) => {
+
   })
 
 program.parse()
